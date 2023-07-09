3e5a9a685ac0cb3e49283f1aaaad90b7a924ee01
Jago Gardiner <jagogardiner@gmail.com>
2023-07-09 23:11:43 +0100

    Adding graph visuals
    Change-Id: I1adea24481aa18959d0e54560657b8297567a2b4
    Signed-off-by: Jago Gardiner <jagogardiner@gmail.com>

--- src/renderer.js
+++ src/renderer.js
@@ -1,6 +1,6 @@
 var sortedPatients;
 var adminPaitentData;
-alert("Please open Paitent Data file.");
+alert("Please open Paitent Data file. Please wait! This may take a while to load.");
 window.electronAPI.readFile().then((data) => {
     // Sort data per patient
     sortedPatients = sortDataPaitents(data);
@@ -8,6 +8,10 @@ window.electronAPI.readFile().then((data) => {
     window.electronAPI.readFile().then((data) => {
         // Sort admin drugs per patient
         adminPaitentData = sortAdminDrugs(sortedPatients, data);
+        // Create charts
+        (async function () {
+            createChartPatient(1097);
+        })();
     });
 });
 function sortAdminDrugs(paitentData, data) {
@@ -97,20 +101,118 @@ function sortDataPaitents(data) {
     return newDataArray;
 }
 
-(async function () {
-    window.electronAPI.createChart(
-        {
-            type: 'line',
-            data: {
-                labels: ["DoB", "Age", "Gender", "Dept", "UnitFromTime", "UnitToTime", "eCaMISDischargeDate", "Abbreviation", "ObsValue", "MarkedInError", "UnitName", "ObsTime", "ObsValidationTime"],
-                datasets: [{
-                    label: 'Patient 2019-2020',
-                    data: retdata,
-                    borderColor: 'rgb(255, 99, 132)',
-                    backgroundColor: 'rgba(255, 99, 132, 0.5)',
-                    tension: 0.1
-                }]
-            }
-        }, "overviewChart"
-    );
-})();
+function createChartPatient(patientID) {
+    var patient = adminPaitentData.find(x => x.PatientIDnew == patientID);
+    var options = {
+        responsive: true,
+        maintainAspectRatio: false,
+        scales: {
+            y: {
+                min: 0,
+                ticks: {
+                    font: {
+                        size: 10
+                    },
+                    stepSize: 10,
+                }
+            },
+        },
+        plugins: {
+            legend: {
+                position: 'right',
+                maxWidth: 400,
+            },
+            tooltip: {
+                enabled: true,
+                callbacks: {
+                    label: () => " label",
+                },
+            },
+        },
+    }
+    var chartData = {
+        type: 'line',
+        options: options,
+        data: {
+            labels: patient.Observations.map((x) => x.ObsTime),
+            datasets: [{
+                label: 'HR',
+                data: patient.Observations.map((x) => x.Abbreviation == "*HR" ? x.ObsValue : null),
+            },
+            {
+                label: 'RR',
+                data: patient.Observations.map((x) => x.Abbreviation == "*RR" ? x.ObsValue : null),
+            },
+            {
+                label: 'SpO2',
+                data: patient.Observations.map((x) => x.Abbreviation == "SpO2" ? x.ObsValue : null),
+            },
+            {
+                label: 'FiO2',
+                data: patient.Observations.map((x) => x.Abbreviation == "*FiO2" ? x.ObsValue : null),
+            },
+            {
+                label: 'Temp. (Overhead)',
+                data: patient.Observations.map((x) => x.Abbreviation == "Temp. (Overhead)" ? x.ObsValue : null),
+            },
+            {
+                label: 'NIBPdias',
+                data: patient.Observations.map((x) => x.Abbreviation == "*NIBPdias" ? x.ObsValue : null),
+            },
+            {
+                label: 'NIBPmean',
+                data: patient.Observations.map((x) => x.Abbreviation == "NIBPmean" ? x.ObsValue : null),
+            },
+            {
+                label: 'NIBPsys',
+                data: patient.Observations.map((x) => x.Abbreviation == "*NIBPsys" ? x.ObsValue : null),
+            },
+            {
+                label: 'Axillary Temp',
+                data: patient.Observations.map((x) => x.Abbreviation == "Axillary Temp" ? x.ObsValue : null),
+            },
+            {
+                label: 'Environment Temp',
+                data: patient.Observations.map((x) => x.Abbreviation == "Environment Temp" ? x.ObsValue : null),
+            },
+            {
+                label: 'Mattress Temp',
+                data: patient.Observations.map((x) => x.Abbreviation == "Mattress Temp" ? x.ObsValue : null),
+            },
+            {
+                label: 'Pinsp (set PIP)',
+                data: patient.Observations.map((x) => x.Abbreviation == "Pinsp (set PIP)" ? x.ObsValue : null),
+            },
+            {
+                label: 'Position',
+                data: patient.Observations.map((x) => x.Abbreviation == "Position" ? x.ObsValue : null),
+            },
+            {
+                label: 'Set PEEP',
+                data: patient.Observations.map((x) => x.Abbreviation == "Set PEEP" ? x.ObsValue : null),
+            },
+            {
+                label: 'Set PEEP or CPAP',
+                data: patient.Observations.map((x) => x.Abbreviation == "Set PEEP or CPAP" ? x.ObsValue : null),
+            },
+            {
+                label: 'Skin Temp',
+                data: patient.Observations.map((x) => x.Abbreviation == "Skin Temp" ? x.ObsValue : null),
+            },
+            {
+                label: 'Vent Measured RR',
+                data: patient.Observations.map((x) => x.Abbreviation == "Vent Measured RR" ? x.ObsValue : null),
+            },
+            {
+                label: 'Ventilation/Support',
+                data: patient.Observations.map((x) => x.Abbreviation == "Ventilation/Support" ? x.ObsValue : null),
+            },
+            {
+                label: 'Ventilation/Mode',
+                data: patient.Observations.map((x) => x.Abbreviation == "Ventilation/Mode" ? x.ObsValue : null),
+            },
+            ]
+        }
+    }
+    window.electronAPI.createChart(chartData, "overviewChart")
+}
