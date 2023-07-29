bf52932216a8b90897d70a35f7f3d5f94261faec
Jago Gardiner <jagogardiner@gmail.com>
2023-07-29 15:21:19 +0100

    Adding logic for chart views.
    Change-Id: I984250ec380b78d3d52aa7681a4e94f7c2c83e27
    Signed-off-by: Jago Gardiner <jagogardiner@gmail.com>

--- src/renderer.js
+++ src/renderer.js
@@ -2,6 +2,7 @@ var sortedPatients;
 var adminPaitentData;
 var patientChart;
 var adminChart;
+var paitentToggleView = true;
 $('#fader').fadeIn(1000);
 $('#loading').css('display', 'flex');
 alert("Please open paitent data file. This may take a while to load.");
@@ -22,21 +23,36 @@ window.electronAPI.readFile().then((data) => {
             select.appendChild(el);
         }
         // Create charts
-        (async function () {
-            handleChart(adminPaitentData[0].PatientIDnew);
-        })();
+        createChartAdministration(adminPaitentData[0].PatientIDnew);
+        createChartPatient(adminPaitentData[0].PatientIDnew);
+        // Hide loading screen
         $('#loading').css('display', 'none');
         $('#fader').fadeOut(1000);
-        $(function () {
-            $("#home-btn").on('click', function () {
-                $("#administrationChart").fadeOut(500);
-                $("#overviewChart").fadeIn(500);
-            });
-            $("#activity-btn").on('click', function () {
-                $("#overviewChart").fadeOut(500);
-                $("#administrationChart").fadeIn(500);
-            });
-        });
+    });
+});
+
+$(function () {
+    // Logic for the sidebar buttons.
+    $("#home-btn").on('click', function () {
+        // Fade out the current chart and fade in the new one
+        $("#administrationChart").fadeOut(500);
+        $("#overviewChart").fadeIn(500);
+        // Set the toggle view to true
+        paitentToggleView = true;
+        // Reset the select box
+        var select = document.getElementById("patientIDList");
+        select.selectedIndex = 0;
+    }
+    );
+    $("#activity-btn").on('click', function () {
+        // Fade out the current chart and fade in the new one
+        $("#overviewChart").fadeOut(500);
+        $("#administrationChart").fadeIn(500);
+        // Set the toggle view to false
+        paitentToggleView = false;
+        // Reset the select box
+        var select = document.getElementById("patientIDList");
+        select.selectedIndex = 0;
     });
 });
 
@@ -416,6 +432,10 @@ function createChartAdministration(patientID) {
 }
 
 function handleChart(patientID) {
-    createChartPatient(patientID);
-    createChartAdministration(patientID);
+    // Get currently focused chart and create the one needed, then destroy the old one.
+    if (paitentToggleView) {
+        createChartPatient(patientID);
+    } else {
+        createChartAdministration(patientID);
+    }
 }
