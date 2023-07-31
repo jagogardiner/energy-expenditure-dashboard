193b010a8e878735bc35b9e7bfbcbabe27ea7499
Jago Gardiner <jagogardiner@gmail.com>
2023-07-31 09:32:48 +0100

    Adding logos for application
    Change-Id: If3a9c8cb820910209ca2a1db46742ba7914c3388
    Signed-off-by: Jago Gardiner <jagogardiner@gmail.com>

--- forge.config.js
+++ forge.config.js
@@ -1,24 +1,39 @@
 module.exports = {
   packagerConfig: {
     asar: true,
+    icon: './src/img/nhs-sq.png',
   },
   rebuildConfig: {},
   makers: [
     {
       name: '@electron-forge/maker-squirrel',
-      config: {},
+      config: {
+        // The ICO file to use as the icon for the generated Setup.exe
+        setupIcon: './src/img/nhs-sq.png'
+      }
     },
     {
       name: '@electron-forge/maker-zip',
       platforms: ['darwin'],
+      config: {
+        icon: './src/img/nhs-sq.png',
+      },
     },
     {
       name: '@electron-forge/maker-deb',
-      config: {},
+      config: {
+        options: {
+          icon: './src/img/nhs-sq.png',
+        }
+      },
     },
     {
       name: '@electron-forge/maker-rpm',
-      config: {},
+      config: {
+        options: {
+          icon: './src/img/nhs-sq.png',
+        }
+      },
     },
   ],
   plugins: [
