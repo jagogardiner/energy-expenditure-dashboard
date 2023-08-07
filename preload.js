836274fcfa6fa3060f552a9cace3b3561cd78341
Jago Gardiner <jagogardiner@gmail.com>
2023-08-07 15:59:40 +0100

    Add help page if data was incorrect.
    Change-Id: Ie71292a0e1a2f3c5e55836731b12b83461cfafb4
    Signed-off-by: Jago Gardiner <jagogardiner@gmail.com>

--- preload.js
+++ preload.js
@@ -3,5 +3,6 @@ const { contextBridge, ipcRenderer } = require('electron')
 contextBridge.exposeInMainWorld('electronAPI', {
     readFile: (filePath) => ipcRenderer.invoke('fs:readFile', filePath),
     quitApp: () => ipcRenderer.send('app:quit'),
-    openHelp: () => ipcRenderer.send('app:openHelp')
+    openHelp: () => ipcRenderer.send('app:openHelp'),
+    openHelpData: () => ipcRenderer.send('app:openHelpData')
 })
