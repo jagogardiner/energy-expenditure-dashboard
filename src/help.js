712a698ac75490f500f8ca5324bd49c2190cf27d
Jago Gardiner <jagogardiner@gmail.com>
2023-08-07 14:38:28 +0100

    Adding initial help view, still to be added upon
    Change-Id: I36de61addaec101a4b21356d35d7538f7b040ddf
    Signed-off-by: Jago Gardiner <jagogardiner@gmail.com>

---
+++ src/help.js
@@ -0,0 +1,38 @@
+$(function () {
+    // Hide all help sections, this javascript is a bit messy
+    $('#help-2').hide();
+    $('#help-3').hide();
+    $('#help-4').hide();
+    $('#help1-btn').click(function () {
+        $('#help-1').fadeIn(500);
+        // Hide all other help sections
+        $('#help-2').hide();
+        $('#help-3').hide();
+        $('#help-4').hide();
+        // Show this help section
+    });
+    $('#help2-btn').click(function () {
+        $('#help-2').fadeIn(500);
+        // Hide all other help sections
+        $('#help-1').hide();
+        $('#help-3').hide();
+        $('#help-4').hide();
+        // Show this help section
+    });
+    $('#help3-btn').click(function () {
+        $('#help-3').fadeIn(500);
+        // Hide all other help sections
+        $('#help-1').hide();
+        $('#help-2').hide();
+        $('#help-4').hide();
+        // Show this help section
+    });
+    $('#help4-btn').click(function () {
+        $('#help-4').fadeIn(500);
+        // Hide all other help sections
+        $('#help-1').hide();
+        $('#help-2').hide();
+        $('#help-3').hide();
+        // Show this help section
+    });
+});
